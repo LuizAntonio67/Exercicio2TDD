20812582760dd346bf971e3676249517bef9cc7d
LuizAntonio67 <luiz.cardozo@ccc.ufcg.edu.br>
2024-03-01 22:54:45 -0300

    redbar_verificaPagamento

--- tdd/src/test/java/com/exercicio2/tdd/BoletoTests.java
+++ tdd/src/test/java/com/exercicio2/tdd/BoletoTests.java
@@ -1,17 +1,30 @@
 package com.exercicio2.tdd;
 
+import java.util.ArrayList;
 import java.util.Date;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BoletoTests {
+    Fatura fatura;
+    Boleto boleto;
+    Pagamento pagamento;
 
     @BeforeEach
     void constructor() {
-        Fatura fatura = new Fatura(new Date(), 100, "Luiz");
-        Boleto boleto = new Boleto(123, new Date(), 100);
-        Pagamento pagamento = new Pagamento(boleto, "BOLETO");
+        this.fatura = new Fatura(new Date(), 100, "Luiz");
+        this.boleto = new Boleto(123, new Date(), 100);
+        this.pagamento = new Pagamento(boleto, "BOLETO");
+    }
+    
+    @Test
+    void verificaCriacaoProcessador() {
+        ArrayList<Boleto> lista = new ArrayList<>();
+        lista.add(boleto);
+        ProcessadorBoletos proc = new ProcessadorBoletos(lista);
     }
 
 }
