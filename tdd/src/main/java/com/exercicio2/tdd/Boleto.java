5b15f35179d736804e951b2cefcd99bbdc5598ee
LuizAntonio67 <luiz.cardozo@ccc.ufcg.edu.br>
2024-03-01 22:16:53 -0300

    refatoramento_boleto

--- tdd/src/main/java/com/exercicio2/tdd/Boleto.java
+++ tdd/src/main/java/com/exercicio2/tdd/Boleto.java
@@ -1,23 +1,16 @@
 package com.exercicio2.tdd;
 
 import java.util.Date;
-import java.util.UUID;
 
 public class Boleto {
-    private UUID id;
     private int codigo;
     private Date data;
     private int valor_pago;
 
     public Boleto(int codigo, Date data, int valor_pago) {
-        this.id = UUID.randomUUID();
         this.codigo = codigo;
         this.data = data;
         this.valor_pago = valor_pago;
-    }
-
-    public UUID getId() {
-        return id;
     }
 
     public int getCodigo() {
