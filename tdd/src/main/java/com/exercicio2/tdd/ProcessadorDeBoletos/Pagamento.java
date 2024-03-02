65c448d106bc91c8c111b76ab25ce253f7531965
LuizAntonio67 <luiz.cardozo@ccc.ufcg.edu.br>
2024-03-02 17:33:25 -0300

    refatoramento_processadorBoletos

--- tdd/src/main/java/com/exercicio2/tdd/ProcessadorDeBoletos/Pagamento.java
+++ tdd/src/main/java/com/exercicio2/tdd/ProcessadorDeBoletos/Pagamento.java
@@ -1,14 +1,20 @@
 package com.exercicio2.tdd.ProcessadorDeBoletos;
 
 public class Pagamento {
+    private Boleto boleto;
     private String tipo;
 
-    public Pagamento(String tipo) {
+    public Pagamento(Boleto boleto, String tipo) {
+        this.boleto = boleto;
         this.tipo = tipo;
     }
 
     public String getTipo() {
         return tipo;
     }
-    
+
+    public Boleto getBoleto() {
+        return boleto;
+    }
+
 }
--- tdd/src/main/java/com/exercicio2/tdd/ProcessadorDeBoletos/ProcessadorBoletos.java
+++ tdd/src/main/java/com/exercicio2/tdd/ProcessadorDeBoletos/ProcessadorBoletos.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProcessadorBoletos {
-    private HashMap<Integer, Pagamento> listaPagamentos;
+    private HashMap<Fatura, ArrayList<Pagamento>> listaFaturas;
 
     public ProcessadorBoletos() {
-        this.listaPagamentos = new HashMap<>();
+        this.listaFaturas = new HashMap<>();
     }
 
     public boolean processamentoBoletos(ArrayList<Boleto> listaBoletos, Fatura fatura) {
         int soma = 0;
-        for (Boleto boleto : listaBoletos) {
-            soma += boleto.getValor_pago();
-            listaPagamentos.put(boleto.getCodigo(), new Pagamento("BOLETO"));
+        for (int i = 0; i < listaBoletos.size(); i++) {
+            if(i == 0) {
+                ArrayList<Pagamento> listaPagamentos = new ArrayList<>();
+                listaPagamentos.add(new Pagamento(listaBoletos.get(i), "BOLETO"));
+                listaFaturas.put(fatura, listaPagamentos);
+            } else {
+                listaFaturas.get(fatura).add(new Pagamento(listaBoletos.get(i), "BOLETO"));
+            }
+            
+            soma += listaBoletos.get(i).getValor_pago();
         }
 
         if(soma >= fatura.getValor()) {
