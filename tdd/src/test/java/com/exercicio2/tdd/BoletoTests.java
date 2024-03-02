8d023b46eca008e95efda82c624e98150b74511e
LuizAntonio67 <luiz.cardozo@ccc.ufcg.edu.br>
2024-03-01 22:39:28 -0300

    refatoramento_testes

--- tdd/src/test/java/com/exercicio2/tdd/BoletoTests.java
+++ tdd/src/test/java/com/exercicio2/tdd/BoletoTests.java
@@ -1,23 +1,17 @@
 package com.exercicio2.tdd;
 
 import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BoletoTests {
-    
-    @Test
-    void verificaCriacaoFatura() {
+
+    @BeforeEach
+    void constructor() {
         Fatura fatura = new Fatura(new Date(), 100, "Luiz");
-    }
-
-    @Test
-    void verificaCriacaoBoleto() {
         Boleto boleto = new Boleto(123, new Date(), 100);
-    }
-
-    @Test
-    void verificaCriacaoPagamento() {
-        Pagamento pagamento = new Pagamento(new Boleto(123, new Date(), 100), "BOLETO");
+        Pagamento pagamento = new Pagamento(boleto, "BOLETO");
     }
 
 }
