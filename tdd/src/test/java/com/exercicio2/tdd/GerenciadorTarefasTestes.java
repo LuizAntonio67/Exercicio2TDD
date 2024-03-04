7e4b66ffce7de5d5687c8147d00df6d5a060cb64
lucasbatista036 <lucas.batista.vieira@ccc.ufcg.edu.br>
2024-03-04 16:59:14 -0300

    Redbar Criar nova tarefa

--- tdd/src/test/java/com/exercicio2/tdd/GerenciadorTarefasTestes.java
+++ tdd/src/test/java/com/exercicio2/tdd/GerenciadorTarefasTestes.java
@@ -1,5 +1,18 @@
 package com.exercicio2.tdd;
 
-public class GerenciadorTarefasTestes {
+class GerenciadorTarefasTestes {
+	
+	 @BeforeEach
+	    void constructor() {
+	        tarefa1 = new Tarefa("Jogar o lixo", "preciso jogar o lixo as 13 horas", "15/12/2024", "Alta");
+	        tarefa2 = new Tarefa("Aparar a grama", "preciso apara a grama as 14 horas", "14/12/2024", "Media");
+	        tarefa3 = new Tarefa("Jogar pingong", "Quero jogar pingpong as 15 horas", "16/12/2024", "Baixa");
+	        
+	        gerenciador = new GerenciadorTarefas();
+	   
+	    }
 
-}
+	    @Test
+	    void verificaCriacaoNovaTarefa() {
+	        assertTrue(gerenciador.adicionaTarefa(tarefa1)); // caso retorne true, a tarefa foi criada com sucesso
+	    }
